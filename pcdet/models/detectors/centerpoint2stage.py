7f0341e79a41cd6e7395c7500731ac7a532314af
YS-Kuang <83287399+YS-Kuang@users.noreply.github.com>
2022-09-16 14:47:41 +0200

    Update centerpoint2stage.py

--- pcdet/models/detectors/centerpoint2stage.py
+++ pcdet/models/detectors/centerpoint2stage.py
@@ -2,7 +2,7 @@ from .detector3d_template import Detector3DTemplate
 import torch
 
 
-class Centerpoint2stage(Detector3DTemplate):
+class CenterPoint2stage(Detector3DTemplate):
     def __init__(self, model_cfg, num_class, dataset):
         super().__init__(model_cfg=model_cfg, num_class=num_class, dataset=dataset)
         self.module_list = self.build_networks()
